618fc63b22454153e9db43d2a54f7d58ab3448db
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-30 17:22:33 -0400

    test

--- R/msg.R
+++ R/msg.R
@@ -9,7 +9,7 @@
 #' @importFrom cli style_bold style_underline col_cyan col_yellow col_magenta
 #' @export
 msg_logical <- function() {
-  sprintf(fmt = 'Some scientists do not understand %s value (e.g., %s being %s/%s)\nConsider using 2-level %s (e.g., %s being %s/%s)',
+  sprintf(fmt = '\nSome scientists do not understand %s value (e.g., %s being %s/%s)\nConsider using 2-level %s (e.g., %s being %s/%s)',
           'logical' |> style_underline() |> style_bold(),
           'arm_intervention' |> col_cyan() |> style_bold(),
           'TRUE' |> col_yellow(), 'FALSE' |> col_yellow(),
@@ -17,4 +17,6 @@ msg_logical <- function() {
           'arm' |> col_magenta() |> style_bold(),
           'intervention' |> col_yellow(), 'control' |> col_yellow())
 }
+
+# add a `\n` upfront to make sure the error message starts on a new line
 
