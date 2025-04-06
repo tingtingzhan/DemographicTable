e413de472d3864fdc040e4fc517a48ee984247df
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-06 15:09:01 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -1,16 +1,4 @@
 
-
-#' @details
-#'
-#' Functions for creating demographic table with simple summary statistics, with optional comparison(s) over one or more groups.
-#' Numeric variables are summarized in means, standard deviations, medians, inter-quartile-ranges (IQR), 
-#' skewness, Shapiro-Wilk normality test and ranges, and compared using two-sample \eqn{t}-test, 
-#' Wilcoxon test, ANOVA and/or Kruskal-Wallis test.
-#' Logical and factor variables are summarized in counts and percentages and
-#' compared using chi-squared test and/or Fisher's exact test.
-#' 
-#' Returned value is printed via package \CRANpkg{flextable}.
-#'
 #' @note
 #' Help files of individual functions are intentionally suppressed in the `pdf` manual.
 #' Users are encouraged to get started with 
